803b4c11d53720aa341fbf666aaa368626d78732
Sirwali Joseph <mulavhe@gmail.com>
2019-02-06 20:49:06 +0200

    Removed the 500's and left the 512

--- server/routes/BusinessController.js
+++ server/routes/BusinessController.js
@@ -363,8 +363,7 @@ router.post("/transactions/for/business/:businessId", auth.required, (req, res, 
         .populate("client", "userName")
         .then(transactions => {
             if (transactions == null) {
-                res.status(500);
-                res.send("Error : 9032rtu834g9erbo");
+                res.status(512).send("Error : 9032rtu834g9erbo");
             }
             res.json(transactions);
         })
