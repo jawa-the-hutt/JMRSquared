8c259b2d8e625337f336b783534bd484289039b5
Sirwali Joseph <jsirwali@retrorabbit.co.za>
2019-02-20 20:08:20 +0200

    Travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-git config --global push.default simple # we only want to push one branch — master
-# specify the repo on the live server as a remote repo, and name it 'production'
-# <user> here is the separate user you created for deploying
-git remote add production ssh://build@travis-ci.org@68.66.193.181:7822/home/mulavhe/www/JMRSqauredDev
-git push production develop # push our updates
+sshpass -p $1 rsync --progress -avz -e ../ ssh -o StrictHostKeyChecking=no $0@$2 -p $3 /home/mulavhe/www/JMRSquaredDev
