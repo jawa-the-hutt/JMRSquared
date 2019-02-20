33f83a7d09a7196a2f87adc9a82b9b9d57a9568c
Sirwali Joseph <jsirwali@retrorabbit.co.za>
2019-02-20 20:22:44 +0200

    Travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sshpass -p $1 rsync --progress -avz -e ./ ssh -o StrictHostKeyChecking=no $0@$2:$3:/home/mulavhe/www/JMRSquaredDev
+sshpass -p $1 rsync --progress -avz ./ ssh $0@$2:$3/home/mulavhe/www/JMRSquaredDev
