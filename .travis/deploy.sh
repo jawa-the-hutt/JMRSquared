cc0b7b10ea2eb40141bc57239b2eb46e37d9285b
Sirwali Joseph <jsirwali@retrorabbit.co.za>
2019-02-20 21:11:40 +0200

    Travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sshpass -p $1 rsync --progress -avzP -e "ssh -p7822" ./ $0@$2:/home/mulavhe/www/JMRSquaredDev
+sshpass -p $1 rsync --progress -avrzP -e "ssh -p7822" ./server $0@$2:/home/mulavhe/www/JMRSquaredDev/server
+sshpass -p $1 rsync --progress -avrzP -e "ssh -p7822" ./client $0@$2:/home/mulavhe/www/JMRSquaredDev/client
