f82eb75fb42bd3d58a0b090ef885df9bdd0ef6b5
Lavhe <jsirwali@retrorabbit.co.za>
2019-02-20 15:30:46 +0200

    Travis

--- bash.sh
+++ bash.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 # mongod --quiet --auth --config /etc/mongod.conf
-npm install
-npm run build
+fuser 3000/tcp -k
+
+npm install --prefix ./server/
+npm run build --prefix  ./server/
+npm start --prefix ./server/
