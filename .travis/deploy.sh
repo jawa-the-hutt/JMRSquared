002a18a27185ae4495287785cd86b9d10cf8162f
Sirwali Joseph <jsirwali@retrorabbit.co.za>
2019-02-20 22:06:11 +0200

    Travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sshpass -p $1 rsync --progress -av --dry-run --update --quiet -e "ssh -p7822" ./* $0@$2:/home/mulavhe/www/JMRSquaredDev
-sshpass -p $1 rsync --progress -av --update -e "ssh -p7822" ./* $0@$2:/home/mulavhe/www/JMRSquaredDev
-sshpass -p $1 ssh $0@$2:/home/mulavhe/www/JMRSquaredDev/bash.sh -p 7822
+sshpass -p $1 rsync --progress -av --dry-run --update --quiet -e "ssh -p7822" ./* $0@$2:/home/mulavhe/www/JMRSquaredDev/
+sshpass -p $1 rsync --progress -av --update -e "ssh -p7822" ./* $0@$2:/home/mulavhe/www/JMRSquaredDev/
+sshpass -p $1 ssh -p 7822 $0@$2:/home/mulavhe/www/JMRSquaredDev/ 'bash -s' < $4
