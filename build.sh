52f9ea8f16622ce7706eeb311f04d13b3f7181bf
Sirwali Joseph <jsirwali@retrorabbit.co.za>
2019-02-27 09:36:23 +0200

    Log running

--- build.sh
+++ build.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-export LC_ALL=C
 echo "Deploying to $1";
 uname -a;
-npm install --prefix "$1/server/";
-npm run build --prefix  "$1/server/";
-fuser "$2/tcp" -k;
-mongod --quiet --auth --config /etc/mongod.conf &
-npm run play --prefix "$1/server/" &
+
+if lsof -Pi :"$2" -sTCP:LISTEN -t >/dev/null ; then
+    echo "Server was already running";
+    npm install --prefix "$1/server/";
+    npm run build --prefix  "$1/server/";
+else
+    echo "Server was has stopped, Restarting it...";
+    npm install --prefix "$1/server/";
+    npm run build --prefix  "$1/server/";
+    date &>> "/tmp/running.txt";
+    npm start --prefix "$1/server/" &>> "/tmp/running.txt" &
+fi
+
 echo "Done deploying to $1";
