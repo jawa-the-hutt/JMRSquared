a96e15da707d23e2c9ba2cf2fe54a4a98015f5d7
Sirwali Joseph <jsirwali@retrorabbit.co.za>
2019-02-27 20:27:29 +0200

    Travis

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-echo "Deploying to $1";
+echo "Deploying to $1" &>> "/tmp/running-$2.txt";
 uname -a;
 
 if lsof -Pi :"$2" -sTCP:LISTEN -t >/dev/null ; then
-    echo "Server was already running";
+    echo "Server was already running" &>> "/tmp/running-$2.txt";
     npm install --prefix "$1/server/";
     npm run build --prefix  "$1/server/";
 else
-    echo "Server was has stopped, Restarting it...";
+    echo "Server was has stopped, Restarting it..." &>> "/tmp/running-$2.txt";
     npm install --prefix "$1/server/";
     npm run build --prefix  "$1/server/";
     date &>> "/tmp/running-$2.txt";
-    npm start --prefix "$1/server/" &>> "/tmp/running.txt" &
+    npm start --prefix "$1/server/" &>> "/tmp/running-$2.txt";
 fi
 
-echo "Done deploying to $1";
+echo "Done deploying to $1" &>> "/tmp/running-$2.txt";
