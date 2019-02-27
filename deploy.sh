8e24e6927788749ebaadfdbb83b5eb75fd82519d
Lavhe <jsirwali@retrorabbit.co.za>
2019-02-27 08:02:05 +0200

    Travis

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+$PASSWORD = $1
+$PORT = $2
+$USERNAME = $3
+$ADDRESS = $4
+$PROJECT_FOLDER = $5
+$PROJECT_PORT = $6
+
+echo $PASSWORD
+# Copy files to server
+sshpass -p "$PASSWORD" rsync --progress -azvh --update --exclude='.git/' --filter=":- .gitignore" --exclude='.travis.yml' -e "ssh -p $PORT -oStrictHostKeyChecking=no" "./" "$USERNAME@$ADDRESS:$PROJECT_FOLDER/"
+
+# Run build on server
+sshpass -p "$PASSWORD" ssh -t -p "$PORT" -oStrictHostKeyChecking=no "$USERNAME@$ADDRESS" "bash -s" < ./build.sh "$PROJECT_FOLDER" "$PROJECT_PORT"
