f6c626ecfafb32e647ac3418886f0292ddad8fb6
Lavhe <jsirwali@retrorabbit.co.za>
2019-02-28 13:32:27 +0200

    Travis

--- build.sh
+++ build.sh
@@ -1,17 +1,1 @@
-#!/bin/bash
-FILENAME=$3;
-echo "Deploying to $1" >> "/tmp/$FILENAME";
-ls;
-
-if lsof -Pi :"$2" -sTCP:LISTEN -t >/dev/null ; then
-    echo "Server was already up" >> "/tmp/$FILENAME";
-    npm install --prefix "$1/server/";
-    npm run build --prefix  "$1/server/";
-else
-    echo "Server was has stopped, Restarting it..." >> "/tmp/$FILENAME";
-    npm install --prefix "$1/server/";
-    npm run build --prefix  "$1/server/";
-    npm start --prefix "$1/server/" >> "/tmp/$FILENAME";
-fi
-
-echo "Done deploying to $1" >> "/tmp/$FILENAME";
+#!/bin/bashFILENAME=$3;echo "Deploying to $1" >> "/tmp/$FILENAME";ls;if lsof -Pi :"$2" -sTCP:LISTEN -t >/dev/null ; then    echo "Server was already up" >> "/tmp/$FILENAME";    npm install --prefix "$1/server/";    npm run build --prefix  "$1/server/";else    echo "Server was has stopped, Restarting it..." >> "/tmp/$FILENAME";    npm install --prefix "$1/server/";    npm run build --prefix  "$1/server/";    npm start --prefix "$1/server/" >> "/tmp/$FILENAME";fiecho "Done deploying to $1" >> "/tmp/$FILENAME";
--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,1 @@
-#!/bin/bash
-PASSWORD =$1;
-PORT=$2;
-USERNAME=$3;
-ADDRESS=$4;
-PROJECT_FOLDER=$5;
-PROJECT_PORT=$6;
-FILE_NAME="running-$2-$(date).txt";
-
-# Copy files to server
-echo "Build file is /tmp/$FILE_NAME";
-echo "Firing a build and deploy to $PROJECT_FOLDER" >> "/tmp/$FILE_NAME";
-sshpass -p "$PASSWORD" rsync --progress -azvh --update --exclude='.git/' --filter=":- .gitignore" --exclude='.travis.yml' -e "ssh -p $PORT -oStrictHostKeyChecking=no" "./" "$USERNAME@$ADDRESS:$PROJECT_FOLDER/";
-
-# Run build on server
-sshpass -p "$PASSWORD" ssh -t -p "$PORT" -oStrictHostKeyChecking=no "$USERNAME@$ADDRESS" "bash -s" < ./build.sh "$PROJECT_FOLDER" "$PROJECT_PORT" "$FILE_NAME";
+#!/bin/bashPASSWORD =$1;PORT=$2;USERNAME=$3;ADDRESS=$4;PROJECT_FOLDER=$5;PROJECT_PORT=$6;FILE_NAME="running-$2-$(date +"%F___%T").txt";# Copy files to serverecho "Build file is /tmp/$FILE_NAME";echo "Firing a build and deploy to $PROJECT_FOLDER" >> "/tmp/$FILE_NAME";# sshpass -p "$PASSWORD" rsync --progress -azvh --update --exclude='.git/' --filter=":- .gitignore" --exclude='.travis.yml' -e "ssh -p $PORT -oStrictHostKeyChecking=no" "./" "$USERNAME@$ADDRESS:$PROJECT_FOLDER/";# Run build on serversshpass -p "$PASSWORD" ssh -t -p "$PORT" -oStrictHostKeyChecking=no "$USERNAME@$ADDRESS" "bash -s" < ./build.sh "$PROJECT_FOLDER" "$PROJECT_PORT" "$FILE_NAME";
