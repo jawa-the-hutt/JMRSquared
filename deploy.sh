c693673527303bf5f805b3013b8c55099891a4e1
Sirwali Joseph <jsirwali@retrorabbit.co.za>
2019-03-04 21:14:22 +0200

    Added -u

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ FILE_NAME="running-$6-$(date +"%F___%T").txt";
 
 # Copy files to server
 echo "Firing a build and deploy to $PROJECT_FOLDER" >> "/tmp/$FILE_NAME";
-sshpass -p "$PASSWORD" rsync --progress -azvh --delete --exclude='.git/' --filter=":- .gitignore" --exclude='.travis.yml' -e "ssh -p $PORT -oStrictHostKeyChecking=no" "./" "$USERNAME@$ADDRESS:$PROJECT_FOLDER/";
+sshpass -p "$PASSWORD" rsync --progress -azvhu --delete --exclude='.git/' --filter=":- .gitignore" --exclude='.travis.yml' -e "ssh -p $PORT -oStrictHostKeyChecking=no" "./" "$USERNAME@$ADDRESS:$PROJECT_FOLDER/";
 
 # Run build on server
 sshpass -p "$PASSWORD" ssh -t -p "$PORT" -oStrictHostKeyChecking=no "$USERNAME@$ADDRESS" "bash -s" < ./build.sh "$PROJECT_FOLDER" "$PROJECT_NAME" "$FILE_NAME";
