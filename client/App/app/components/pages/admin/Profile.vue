9ffdb2382bbc913b1c4166b9feab61d279e661e6
Sirwali Joseph <jsirwali@retrorabbit.co.za>
2019-03-12 07:32:06 +0200

    Added select profile pic

--- client/App/app/components/pages/admin/Profile.vue
+++ client/App/app/components/pages/admin/Profile.vue
@@ -2,15 +2,18 @@
   <page backgroundSpanUnderStatusBar actionBarHidden="true">
     <GridLayout rows="auto,auto,*" columns="*,auto">
       <StackLayout class="m-t-5" textAlignment="center" colSpan="2" verticalAlignment="center" row="0">
-        <Image textAlignment="center" stretch="aspectFill" width="100" height="100" borderRadius="100%" :src="user.profilePic ? user.profilePic : $store.state.settings.defaultProfilePic"></Image>
+        <Ripple @tap="choosePic()">
+          <ActivityIndicator textAlignment="center" verticalAlignment="center" v-if="!profilePic" :busy="true"></ActivityIndicator>
+          <Image textAlignment="center" v-if="profilePic" stretch="aspectFill" width="100" height="100" borderRadius="100%" :src="profilePic"></Image>
+        </Ripple>
         <label row="0" class="labelTitle m-t-5" textAlignment="center">{{ user.fullName }}</label>
         <label row="0" class="labelname" textAlignment="center">{{ user.userName }}</label>
       </StackLayout>
       <CardView col="1" row="1" textAlignment="right" elevation="15" radius="100" margin="10">
-      <Ripple @tap="isEditting = !isEditting">
-        <label class="mdi p-15 black-text" verticalAlignment="center" textAlignment="left" fontSize="35" :text="(isEditting ? 'mdi-close' : 'mdi-pencil') | fonticon"></label>
-      </Ripple>
-     </CardView>
+        <Ripple @tap="isEditting = !isEditting">
+          <label class="mdi p-15 black-text" verticalAlignment="center" textAlignment="left" fontSize="35" :text="(isEditting ? 'mdi-close' : 'mdi-pencil') | fonticon"></label>
+        </Ripple>
+      </CardView>
       <StackLayout colSpan="2" row="2">
         <CardView v-if="!isEditting" textAlignment="center" margin="10">
           <ScrollView width="100%">
@@ -33,19 +36,19 @@
         </CardView>
         <CardView v-if="isEditting" textAlignment="center" margin="10">
           <GridLayout rows="*,auto">
-          <ScrollView row="0" width="100%">
-            <StackLayout width="100%">
-              <GridLayout class="text-dark-black p-x-20 p-y-15" rows="auto,auto" columns="auto,*" v-for="(userInfo,index) in userInfos" :key="index">
-                <label row="0" col="0" class="mdi text-dark-black m-r-20" rowSpan="2" verticalAlignment="center" textAlignment="left" fontSize="35" :text="userInfo.icon | fonticon"></label>
-                <label row="0" col="1" class="font-weight-bold" fontSize="15" verticalAlignment="center" textAlignment="left" :text="userInfo.title"></label>
-                <TextField row="1" col="1" fontSize="15" textAlignment="left" v-model="userInfo.body" />
-              </GridLayout>
-            </StackLayout>
-          </ScrollView> 
-            <StackLayout row="1">
-                <Button text="Save changes" :isEnabled="!isLoading" class="submit-button bg-dark-blue text-white"></Button>
+            <ScrollView row="0" width="100%">
+              <StackLayout width="100%">
+                <GridLayout class="text-dark-black p-x-20 p-y-15" rows="auto,auto" columns="auto,*" v-for="(userInfo,index) in userInfos" :key="index">
+                  <label row="0" col="0" class="mdi text-dark-black m-r-20" rowSpan="2" verticalAlignment="center" textAlignment="left" fontSize="35" :text="userInfo.icon | fonticon"></label>
+                  <label row="0" col="1" class="font-weight-bold" fontSize="15" verticalAlignment="center" textAlignment="left" :text="userInfo.title"></label>
+                  <TextField row="1" col="1" fontSize="15" textAlignment="left" v-model="userInfo.body" />
+                </GridLayout>
               </StackLayout>
-              </GridLayout>
+            </ScrollView>
+            <StackLayout row="1">
+              <Button text="Save changes" :isEnabled="!isLoading" class="submit-button bg-dark-blue text-white"></Button>
+            </StackLayout>
+          </GridLayout>
         </CardView>
       </StackLayout>
     </GridLayout>
@@ -57,6 +60,7 @@ const dialogs = require("ui/dialogs");
 import * as Toast from "nativescript-toast";
 
 import * as imagepicker from "nativescript-imagepicker";
+import * as imageSource from "tns-core-modules/image-source";
 
 var appSettings = require("application-settings");
 import * as connectivity from "tns-core-modules/connectivity";
@@ -73,7 +77,8 @@ export default {
       selectedScreen: 1,
       feeds: [],
       userInfos: [],
-      Devices: []
+      Devices: [],
+      profilePic: null
     };
   },
   computed: {
@@ -97,23 +102,6 @@ export default {
     if (!this.isLoaded) {
       this.pageLoaded();
     }
-    setTimeout(() => {
-      const testing = this.TNS_ENV !== "production";
-      this.$firebase.admob
-        .showBanner({
-          size: this.$firebase.admob.AD_SIZE.SMART_BANNER, // see firebase.admob.AD_SIZE for all options
-          margins: {
-            top: 10
-          },
-          androidBannerId: "ca-app-pub-4924835910036108~3001656373",
-          iosBannerId: "ca-app-pub-4924835910036108~3001656373",
-          testing: testing, // when not running in production set this to true, Google doesn't like it any other way
-          iosTestDeviceIds: [],
-          keywords: ["business", "money", "cash", "rich", "free", "job", "work"] // add keywords for ad targeting
-        })
-        .then(() => {})
-        .catch(errorMessage => {});
-    }, 5000);
   },
   methods: {
     pageLoaded(args = null) {
@@ -137,7 +125,7 @@ export default {
         },
         {
           title: "Contact number",
-          body:"0" + this.user.numbers,
+          body: "0" + this.user.numbers,
           icon: "mdi-phone"
         },
         {
@@ -146,6 +134,10 @@ export default {
           icon: "mdi-clock-outline"
         }
       ];
+
+      this.profilePic = this.user.profilePic
+        ? this.user.profilePic
+        : this.$store.state.settings.defaultProfilePic;
 
       this.Devices = [
         {
@@ -164,74 +156,6 @@ export default {
           manufacturer: "Samsung"
         }
       ];
-
-      this.$api
-        .getAllBusinessesForUser(this.$store.state.cache.cachedAdmin._id)
-        .then(results => {
-          var count = results.length;
-          if (count > this.layouts.length) {
-            count = this.layouts.length;
-          }
-          var i = 0;
-          var tank = [];
-          var timer = setInterval(() => {
-            if (tank.filter(t => t == i).length == 0) {
-              if (i < count) {
-                this.layouts[i].icon = results[i].type.icon;
-                this.layouts[i].title = results[i].name;
-                this.layouts[i].link = "/business/home";
-                this.layouts[i].props = {
-                  businessID: results[i]._id
-                };
-              } else if (i == count && count < this.layouts.length - 1) {
-                if (this.layouts.filter(l => !l.title && !l.icon).length > 0) {
-                  var first = this.layouts.filter(l => !l.title && !l.icon)[0];
-                  first.icon = "briefcase-plus";
-                  first.title = "Add Business";
-                  first.link = "/business/add/business";
-                }
-                this.layouts
-                  .filter(l => !l.title && !l.icon)
-                  .map(layout => {
-                    layout.icon = " ";
-                    layout.title = " ";
-                  });
-                if (args) {
-                  args.object.refreshing = false;
-                }
-              } else {
-                clearInterval(timer);
-              }
-              tank.push(i);
-              i++;
-              this.$forceUpdate();
-            }
-          }, 300);
-        })
-        .catch(err => {
-          this.$feedback.error({
-            title: "Unable to load your businesses",
-            duration: 4000,
-            message: "Please try again later"
-          });
-          this.layouts.forEach(layout => {
-            layout.icon = "bug-report";
-            layout.title = "Invalid";
-          });
-          if (args) {
-            args.object.refreshing = false;
-          }
-          this.$forceUpdate();
-        });
-
-      var firstTime = appSettings.getBoolean("shownChangeLog");
-      if (!firstTime) {
-        this.$feedback.success({
-          title: "Welcome to the JMRSquared app",
-          message: "You are currently using version 1"
-        });
-        appSettings.setBoolean("shownChangeLog", true);
-      }
     },
     refreshList(args) {
       this.pageLoaded(args);
@@ -246,7 +170,44 @@ export default {
         this.navigate(item.link, item.props);
       }
     },
-    onBusinessTap(item) {}
+    onBusinessTap(item) {},
+    choosePic() {
+      let context = imagepicker.create({
+        mode: "single" // use "multiple" for multiple selection
+      });
+      var tempPic = this.profilePic;
+      this.profilePic = null;
+      context
+        .authorize()
+        .then(function() {
+          return context.present();
+        })
+        .then(selection => {
+          selection.forEach(async selected => {
+            // process the selected image
+            let source = new imageSource.ImageSource();
+            let img = await source.fromAsset(selected);
+            if (img) {
+              this.profilePic =
+                "data:image/png;base64," + img.toBase64String("png");
+            }
+          });
+          if (!this.profilePic) {
+            this.profilePic = tempPic;
+          }
+        })
+        .catch(err => {
+          // process error
+          this.$feedback.error({
+            title: "No file selected",
+            message: "Please select a valid image file",
+            duration: 4000,
+            position: 1,
+            onTap: () => {}
+          });
+          this.profilePic = tempPic;
+        });
+    }
   }
 };
 </script>
